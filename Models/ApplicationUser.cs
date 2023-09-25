1f6b421122ec694b7539509ba4ef3ccf0a4e7bb0
Kipras Saparauskas <kipras.saparauskas@gmail.com>
2023-09-25 10:55:33 +0300

    Add First Name, Last Name and Student ID properties to ApplicationUser class. Add properties to ApplicationDbContex.cs and update Registration view and controller for user profile fields

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlineNotes.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StudentId { get; set; }
+    }
+}
