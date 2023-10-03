b9b8a7aadd8afa8c079caea4a73307d334bc5ea0
Vabalas <vabalas.balys@gmail.com>
2023-10-03 14:52:09 +0300

    Created OpenAI api controller and view

---
+++ Controllers/HelpController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineNotes.Models;
+using OpenAI_API;
+
+namespace OnlineNotes.Controllers
+{
+
+    public class HelpController : Controller
+    {
+        private readonly string OpenAIapiKey = "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";
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> HelpButtonAction(string input)
+        {
+            string completionResult = await CompleteSentence(OpenAIapiKey, input);
+
+            return Content(completionResult, "text/plain");
+        }
+
+        public async Task<string> CompleteSentence(string OpenAIapiKey, string input)
+        {
+            var api = new OpenAI_API.OpenAIAPI(OpenAIapiKey);
+            var result = await api.Completions.GetCompletion(input);
+            return result;
+        }
+    }
+}
