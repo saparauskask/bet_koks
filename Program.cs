35c3aa6ea1d134a19aec8f29afbf643d64c80809
Dominykas Misius <misiusd1@gmail.com>
2023-10-04 19:03:53 +0300

    Removed unnecessary pages
    Removed the "Home" pages, new landing page is "Notes/Index", removed unnecessary links from the header.

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ namespace OnlineNotes
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Notes/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -44,7 +44,7 @@ namespace OnlineNotes
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Notes}/{action=Index}/{id?}");
             app.MapRazorPages();
 
             app.Run();
