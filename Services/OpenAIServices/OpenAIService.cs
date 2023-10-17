3ecc2622d986f9161fbd4adbb6117ddde03ef451
Vabalas <vabalas.balys@gmail.com>
2023-10-17 15:29:39 +0300

    Added an default help input using named and optional arguments

--- Services/OpenAIServices/OpenAIService.cs
+++ Services/OpenAIServices/OpenAIService.cs
@@ -13,12 +13,26 @@ namespace OnlineNotes.Services.OpenAIServices
             api = new OpenAIAPI(apiKey?.Key);
         }
 
-        public async Task<string> CompleteSentence(string input)
+        public async Task<string> CompleteHelpRequest(string input = "Can you help me?")
         {
             try
             {
-                string? result = await api.Completions.GetCompletion(input);
-                return result;
+                var result = await api.Completions.CreateCompletionAsync(input, temperature: 1.0);
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "Something went wrong, the request could not be completed";
+            }
+        }
+
+        public async Task<string> CompleteSentence(string input = "Can you help me?")
+        {
+            try
+            {
+                var result = await api.Completions.CreateCompletionAsync(input, temperature: 0.5);
+                return result.ToString();
             }
             catch (Exception ex) 
             {
