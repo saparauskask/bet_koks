e503b563c523c1c253e6b617fae794099f359a6f
Vabalas <vabalas.balys@gmail.com>
2023-10-17 14:10:18 +0300

    Moved file streaming to seperate repository and moved openAiApiKeyRecord to seperate file

--- Services/OpenAIServices/OpenAIService.cs
+++ Services/OpenAIServices/OpenAIService.cs
@@ -1,16 +1,15 @@
-﻿using OpenAI_API;
-using System.Globalization;
+﻿using OnlineNotes.Data;
+using OpenAI_API;
 
 namespace OnlineNotes.Services.OpenAIServices
 {
     public class OpenAIService : IOpenAIService
     {
         readonly OpenAIAPI api;
-        private record OpenAIAPIKey(string Key, DateTime CreationDate);
 
         public OpenAIService()
         {
-            var apiKey = ReadApiKey();
+            var apiKey = FileRepository.ReadApiKey();
             api = new OpenAIAPI(apiKey?.Key);
         }
 
@@ -26,41 +25,6 @@ namespace OnlineNotes.Services.OpenAIServices
                 Console.WriteLine(ex.Message);
                 return "Something went wrong, the request could not be completed";
             }
-        }
-
-       private OpenAIAPIKey? ReadApiKey()
-       {
-            string apiKeyFilePath = ".env";
-            try
-            {
-                using (FileStream fileStream = new FileStream(apiKeyFilePath, FileMode.Open, FileAccess.Read))
-                using (StreamReader reader = new StreamReader(fileStream))
-                {
-                    string apiKey = reader.ReadLine()?.Trim() ?? string.Empty; //provides empty string as default value
-                    string dateStr = reader.ReadLine()?.Trim() ?? string.Empty;
-
-                    if (!string.IsNullOrEmpty(apiKey) && !string.IsNullOrEmpty(dateStr))
-                    {
-                        if (DateTime.TryParseExact(dateStr, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime creationDate))
-                        {
-                            return new OpenAIAPIKey(apiKey, creationDate);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Failed to parse the date in the file.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("API Key or Date is empty or not found in the file.");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return null;
         }
     }
 }
