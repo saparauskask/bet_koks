32e6208d9e037a43438eaced06488f1742824400
Kipras Saparauskas <kipras.saparauskas@gmail.com>
2023-10-21 18:16:40 +0300

    Created string extension class

---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineNotes.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+
+        public static int WordCount(this string contents)
+        {
+            int count = 0;
+            return count;
+        }
+    }
+}
