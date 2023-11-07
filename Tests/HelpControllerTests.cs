4482094780f137ed05267fec635ff35c49106b0a
Vabalas <vabalas.balys@gmail.com>
2023-11-07 13:16:52 +0200

    Tried to create a first test that tests HelpController

---
+++ Tests/HelpControllerTests.cs
@@ -0,0 +1,34 @@
+﻿/*
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NuGet.ContentModel;
+using OnlineNotes.Controllers;
+using OnlineNotes.Services.OpenAIServices;
+using Xunit;
+
+namespace OnlineNotes.Tests
+{
+    public class HelpControllerTests
+    {
+        [Fact]
+        public async Task SendMessageAsync_ValidMessage_ReturnsContentResult()
+        {
+            // Arrange
+            var chatBotServiceMock = new Mock<ChatBotService>();
+            chatBotServiceMock.Setup(service => service.GenerateResponse(It.IsAny<string>()))
+                .ReturnsAsync("MockedResponse");
+
+            var controller = new HelpController(chatBotServiceMock.Object);
+
+            // Act
+            var result = await controller.SendMessageAsync("TestMessage") as ContentResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("MockedResponse", result.Content);
+            Assert.Equal("text/plain", result.ContentType);
+        }
+    }
+}
+*/
