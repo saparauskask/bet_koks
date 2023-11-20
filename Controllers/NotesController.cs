eb96acf1388d245c9668c99bbc3cfad57b6b5909
Kipras Saparauskas <kipras.saparauskas@gmail.com>
2023-11-20 20:57:53 +0200

    Implemented custom NoteAccessDeniedException

--- Controllers/NotesController.cs
+++ Controllers/NotesController.cs
@@ -10,6 +10,8 @@ using OnlineNotes.Services.OpenAIServices;
 using OnlineNotes.Services.RatingServices;
 using Microsoft.AspNetCore.Identity;
 using OnlineNotes.Models.Requests.NoteRating;
+using OnlineNotes.Exceptions;
+using OnlineNotes.Data.Migrations;
 
 namespace OnlineNotes.Controllers
 {
@@ -20,17 +22,19 @@ namespace OnlineNotes.Controllers
         private readonly INotesService _notesService;
         private readonly INoteRatingService _ratingService;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<NotesService> _logger;
 
-        public NotesController(IOpenAIService openAIService, INotesService notesService, INoteRatingService ratingService, UserManager<IdentityUser> userManager)
+        public NotesController(IOpenAIService openAIService, INotesService notesService, INoteRatingService ratingService, UserManager<IdentityUser> userManager, ILogger<NotesService> logger)
         {
             _openAIService = openAIService;
             _notesService = notesService;
             _ratingService = ratingService;
             _userManager = userManager;
+            _logger = logger;
         }
 
         // GET: Notes
-        public async Task<IActionResult> Index(int page = 1)
+        public async Task<IActionResult> Index(int page = 1, string? errorMessage = null)
         {
             IdentityUser user = await _userManager.GetUserAsync(User);
             var userId = user.Id;
@@ -49,23 +53,38 @@ namespace OnlineNotes.Controllers
             // Pagination:
             var data = _notesService.GetPagedNotes(notes, page, this);
 
-
+            ViewBag.ErrorMessage = errorMessage;
             return View(data);
         }
 
         // GET: Notes/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int? id) // TODO exception
         {
-            var note = await _notesService.GetNoteAsync(id);
-
-            if (note == null)
+            try
             {
-                return NotFound();
+                IdentityUser user = await _userManager.GetUserAsync(User);
+                var userId = user.Id;
+
+                var note = await _notesService.GetNoteAsync(id);
+
+                if (note == null)
+                {
+                    return NotFound();
+                }
+                var wordCount = note.Contents.WordCount();
+                ViewBag.Count = wordCount;
+                ViewBag.NoteId = id;
+                if (note.Status == NoteStatus.Draft && userId != note.UserId)
+                {
+                    throw new NoteAccessDeniedException(userId, note.Id, "read");
+                }
+                return View(note);
             }
-            var wordCount = note.Contents.WordCount();
-            ViewBag.Count = wordCount;
-            ViewBag.NoteId = id;
-            return View(note);
+            catch (NoteAccessDeniedException ex)
+            {
+                _logger.LogError($"Access to the note (Id: {ex.NoteId}) during {ex.Operation} operation was denied");
+                return RedirectToAction(nameof(Index), new {errorMessage = ex.GetErrorMessage()});
+            }
         }
 
         // GET: Notes/Create
@@ -122,12 +141,28 @@ namespace OnlineNotes.Controllers
         // GET: Notes/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            var note = await _notesService.GetNoteAsync(id);
-            if (note == null)
+            try
             {
-                return NotFound();
+                IdentityUser user = await _userManager.GetUserAsync(User);
+                var userId = user.Id;
+                var note = await _notesService.GetNoteAsync(id);
+                if (note == null)
+                {
+                    return NotFound();
+                }
+                if (note.Status == NoteStatus.Draft && userId != note.UserId)
+                {
+                    throw new NoteAccessDeniedException(userId, note.Id, "edit");
+                }
+
+                return View(note);
             }
-            return View(note);
+            catch (NoteAccessDeniedException ex)
+            {
+                _logger.LogError($"Access to the note (Id: {ex.NoteId}) during {ex.Operation} operation was denied");
+                return RedirectToAction(nameof(Index), new { errorMessage = ex.GetErrorMessage() });
+            }
+            
         }
 
         public async Task<IActionResult> ExplainTask(string input)
@@ -157,13 +192,28 @@ namespace OnlineNotes.Controllers
         // GET: Notes/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            var note = await _notesService.GetNoteAsync(id);
-
-            if (note == null)
+            try
             {
-                return NotFound();
+                IdentityUser user = await _userManager.GetUserAsync(User);
+                var userId = user.Id;
+                var note = await _notesService.GetNoteAsync(id);
+
+                if (note == null)
+                {
+                    return NotFound();
+                }
+
+                if (note.Status == NoteStatus.Draft && userId != note.UserId)
+                {
+                    throw new NoteAccessDeniedException(userId, note.Id, "delete");
+                }
+                return View(note);
             }
-            return View(note);
+            catch(NoteAccessDeniedException ex)
+            {
+                _logger.LogError($"Access to the note (Id: {ex.NoteId}) during {ex.Operation} operation was denied");
+                return RedirectToAction(nameof(Index), new { errorMessage = ex.GetErrorMessage() });
+            }
         }
 
         // POST: Notes/Delete/5
