83c9464a98db3955a16c1ba777e4a845c10bf88e
Vabalas <vabalas.balys@gmail.com>
2023-11-22 19:01:06 +0200

    Tested OpenAIServices

--- OnlineNotes.Tests/ServicesTests/OpenAIServicesTests/ChatBotServiceTests.cs
+++ OnlineNotes.Tests/ServicesTests/OpenAIServicesTests/ChatBotServiceTests.cs
@@ -14,12 +14,14 @@ using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
 using FakeItEasy;
 using OnlineNotes.Data.ChatHistorySaver;
+using Microsoft.EntityFrameworkCore.Update;
 
 namespace OnlineNotes.Tests.ServicesTests.OpenAIServicesTests
 {
     public class ChatBotServiceTests
     {
         private readonly ChatBotService _chatBotService;
+        private readonly ChatHistorySaver _chatHistorySaver;
 
         public ChatBotServiceTests()
         {
@@ -27,31 +29,92 @@ namespace OnlineNotes.Tests.ServicesTests.OpenAIServicesTests
                 .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
                 .Options;
             var context = new ApplicationDbContext(options);
-            ChatHistorySaver.Initialize(context);
+            try 
+            {
+                ChatHistorySaver.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            _chatHistorySaver = ChatHistorySaver.Instance;
             _chatBotService = new ChatBotService();
         }
 
         [Theory]
-        [InlineData("test")]
-        public void ChatBotService_AddUserMessageAsync_MessageCountplusOne(string text)
+        [InlineData("human test")]
+        public void ChatBotService_AddUserMessage_MessageCountplusOne(string text)
         {
             // Arrange
-            var messagesInDbCountBefore = _chatBotService.GetChatHistory().Count;
+            int messagesInDbCountBefore = _chatBotService.GetChatHistory().Count;
             // Act
             _chatBotService.AddUserMessage(text);
-            var messageInDbCountAfter = _chatBotService.GetChatHistory().Count;
+            int messageInDbCountAfter = _chatHistorySaver.GetPendingMessages().Count;
+            _chatHistorySaver.ClearChatHistory();
             // Assert
+            Assert.True(messageInDbCountAfter > messagesInDbCountBefore);
+        }
+        
+        [Theory]
+        [InlineData("robot test")]
+        public void ChatBotService_AddAIMessage_MessageCountplusOne(string text)
+        {
+            // Arrange
+            int messagesInDbCountBefore = _chatBotService.GetChatHistory().Count;
+            // Act
+            _chatBotService.AddAIMessage(text);
+            int messageInDbCountAfter = _chatHistorySaver.GetPendingMessages().Count;
+            _chatHistorySaver.ClearChatHistory();
+            // Assert
+            Assert.True(messageInDbCountAfter > messagesInDbCountBefore);
+        }
+
+        [Theory]
+        [InlineData("hello!")]
+        public void ChatBotService_GenerateResponse_returnsString(string text)
+        {
+            // Arrange
+            int messagesInDbCountBefore = _chatBotService.GetChatHistory().Count;
+            // Act
+            var result = _chatBotService.GenerateResponse(text);
+            int messageInDbCountAfter = _chatHistorySaver.GetPendingMessages().Count;
+            _chatHistorySaver.ClearChatHistory();
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(messagesInDbCountBefore < messageInDbCountAfter);
         }
 
         [Fact]
-        public void GenerateResponseAsync_AddsUserMessageAndAIMessage()
+        public void ChatBotService_ClearChatHistory_IsZero()
         {
             // Arrange
-
+            _chatBotService.AddUserMessage("1");
+            _chatBotService.AddAIMessage("2");
+            int messagesInDbCountBefore = _chatHistorySaver.GetPendingMessages().Count;
             // Act
-
+            _chatBotService.ClearChatHistory();
+            int messageInDbCountAfter = _chatBotService.GetChatHistory().Count;
             // Assert
+            Assert.True(messageInDbCountAfter == 0);
+            Assert.True(messagesInDbCountBefore > messageInDbCountAfter);
         }
+
+        [Fact]
+        public void ChatBotService_LoadChatHistory_NotNull()
+        {
+            // Arrange
+            var messageList = new List<ChatGptMessage>();
+            messageList.Add(new ChatGptMessage { Content = "x is equal to 2", IsUser = false });
+            messageList.Add(new ChatGptMessage { Content = "x is equal to 2", IsUser = true });
+            // Act
+            _chatBotService.LoadChatHistory(messageList);
+            var result = _chatBotService.GenerateResponse("What is x + x?");
+            // Assert
+            Assert.NotNull(result);
+            //Assert.Contains("4", result.ToString());
+        }
+
+
     }
 
 
