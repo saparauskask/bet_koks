8ffa8bbfa25f6d3ac74d92553d05fba6996393ac
Vabalas <vabalas.balys@gmail.com>
2023-11-28 17:29:18 +0200

    chess daily puzzle

---
+++ OnlineNotes/Controllers/ChessController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineNotes.Controllers
+{
+    public class ChessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
