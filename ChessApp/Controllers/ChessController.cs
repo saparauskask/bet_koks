4224648ca1d6202b14cb1c5480d4df859f4e983b
Vabalas <vabalas.balys@gmail.com>
2023-11-29 15:39:32 +0200

    Created ChessController

---
+++ ChessApp/Controllers/ChessController.cs
@@ -0,0 +1,21 @@
+﻿using ChessApp.ChessLogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChessApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChessController : Controller
+    {
+        public ChessController()
+        {
+        }
+
+        [HttpGet(Name = "GetBoard")]
+        public IActionResult GetBoard()
+        {
+            var board = new Board();
+            return Json(board.GetBoardCopy().ToString());
+        }
+    }
+}
