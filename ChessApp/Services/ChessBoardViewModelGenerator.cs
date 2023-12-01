ccd99dce4189fd9a7150de229b6a77590a563a8c
Vabalas <vabalas.balys@gmail.com>
2023-12-01 21:50:24 +0200

    created html view model for the board

---
+++ ChessApp/Services/ChessBoardViewModelGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using ChessApp.ChessLogic;
+using ChessApp.ChessLogic.Pieces;
+
+namespace ChessApp.Services
+{
+    public class ChessBoardViewModelGenerator
+    {
+        private readonly string htmlStart = "<table class=\"chess-board\"><tbody>";
+        private readonly string htmlEnd = "</tbody></table>";
+        
+        public string GenerateHtml(Board board)
+        {
+            var strB = new StringBuilder();
+            var sq = board.GetBoardCopy().Squares;
+            var sqP = new SquarePainter(true);
+            strB.Append(htmlStart);
+
+            for (int y = 7; y >= 0; --y)
+            {
+                strB.AppendLine("<tr>");
+                for (int x = 0; x < 8; ++x)
+                {
+                    var currentSquare = sq[x, y];
+                    switch (currentSquare.Piece)
+                    {
+                        case Pawn pawn:
+                            strB.AppendLine($"<td class={sqP.currentColor}>{sqP.GetPawn(pawn.IsWhite)}</td>");
+                            break;
+                        case Rook rook:
+                            strB.AppendLine($"<td class={sqP.currentColor}>{sqP.GetPawn(rook.IsWhite)}</td>");
+                            break;
+                        case Knight knight:
+                            strB.AppendLine($"<td class={sqP.currentColor}>{sqP.GetKnight(knight.IsWhite)}</td>");
+                            break;
+                        case Bishop bishop:
+                            strB.AppendLine($"<td class={sqP.currentColor}>{sqP.GetBishop(bishop.IsWhite)}</td>");
+                            break;
+                        case Queen queen:
+                            strB.AppendLine($"<td class={sqP.currentColor}>{sqP.GetQueen(queen.IsWhite)}</td>");
+                            break;
+                        case King king:
+                            strB.AppendLine($"<td class={sqP.currentColor}>{sqP.GetKing(king.IsWhite)}</td>");
+                            break;
+                        default:
+                            strB.AppendLine($"<td class={sqP.currentColor}></td>");
+                            break;
+                    }
+
+                    if (x != 7)
+                    {
+                        sqP.switchColor();
+                    }
+                }
+                strB.AppendLine("</tr>");
+            }
+
+            strB.Append(htmlEnd);
+
+            return strB.ToString();
+        }
+
+
+    }
+}
---
+++ ChessApp/Services/SquarePainter.cs
@@ -0,0 +1,69 @@
+﻿namespace ChessApp.Services
+{
+    public class SquarePainter
+    {
+        public bool IsWhite { get; set; }
+        public string currentColor;
+        private string WPawn = "\u2659";
+        private string WRook = "\u2656";
+        private string WKnight = "\u2658";
+        private string WBishop = "\u2657";
+        private string WQueen = "\u2655";
+        private string WKing = "\u2654";
+        
+        private string BPawn = "\u265f";
+        private string BRook = "\u265c";
+        private string BKnight = "\u265e";
+        private string BBishop = "\u265d";
+        private string BQueen = "\u265b";
+        private string BKing = "\u265a";
+
+        private const string dark = "\"dark\"";
+        private const string light = "\"light\"";
+
+        public SquarePainter(bool startingColor)
+        {
+            IsWhite = startingColor;
+            currentColor = IsWhite ? light : dark;
+        }
+
+        public void switchColor()
+        {
+            if (IsWhite)
+            {
+                currentColor = dark;
+                IsWhite = !IsWhite;
+            }
+            else
+            {
+                currentColor = light;
+                IsWhite = !IsWhite;
+            }
+        }
+
+        public string GetPawn(bool color)
+        {
+            return color ? WPawn : BPawn;
+        }
+        public string GetRook(bool color)
+        {
+            return color ? WRook : BRook;
+        }
+        public string GetKnight(bool color)
+        {
+            return color ? WKnight : BKnight;
+        }
+        public string GetBishop(bool color)
+        {
+            return color ? WBishop : BBishop;
+        }
+        public string GetKing(bool color)
+        {
+            return color ? WKing : BKing;
+        }
+        public string GetQueen(bool color)
+        {
+            return color ? WQueen: BQueen;
+        }
+    }          
+}
