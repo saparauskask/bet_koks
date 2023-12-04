e6633362e2455b2a69b6ed5e5c3d63836f97de76
Vabalas <vabalas.balys@gmail.com>
2023-12-04 22:02:00 +0200

    Created chess controller and view that interacts with web service

--- OnlineNotes/Controllers/ChessController.cs
+++ OnlineNotes/Controllers/ChessController.cs
@@ -1,12 +1,87 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Castle.Components.DictionaryAdapter.Xml;
+using ChessApp.Requests;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace OnlineNotes.Controllers
 {
     public class ChessController : Controller
     {
-        public IActionResult Index()
+        private readonly Uri baseAddress = new Uri("https://localhost:7089/api");
+        private readonly HttpClient _client;
+
+        public ChessController()
         {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            string htmlBoard = "";
+
+            using (HttpResponseMessage response = await _client.GetAsync(_client.BaseAddress + "/ChessApi/CreateGame"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    htmlBoard = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    Console.WriteLine("Error! Chess board could not have been retrieved.");
+                }
+            }
+
+            ViewBag.HtmlBoard = htmlBoard;
+
             return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> RetrieveCoordinates(int fromX, int fromY, int toX, int toY)
+        {
+            Console.WriteLine($"-------Coordinates: ({fromX},{fromY}) to ({toX},{toY})");
+
+            try
+            {
+                var moveData = new MoveRequest
+                {
+                    FromX = fromX,
+                    FromY = fromY,
+                    ToX = toX,
+                    ToY = toY
+                };
+
+                var content = new StringContent(JsonConvert.SerializeObject(moveData), Encoding.UTF8, "application/json");
+
+                using (HttpResponseMessage response = await _client.PostAsync(_client.BaseAddress + "/ChessApi/MakeMove", content))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string updatedHtmlBoard = await response.Content.ReadAsStringAsync();
+
+                        //ViewBag.HtmlBoard = updatedHtmlBoard;
+                        
+                        return Ok(updatedHtmlBoard);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error! Move could not have been made.");
+                        // Handle the error appropriately
+                        return BadRequest(); // Or return an appropriate status code
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"An error occurred: {ex.Message}");
+                // Handle the exception appropriately
+                return StatusCode(500); // Or return an appropriate status code
+            }
         }
     }
 }
