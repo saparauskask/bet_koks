ba76fe3f4d5d5e7801edb48e0ae1c97901a9bb9a
Dominykas Misius <misiusd1@gmail.com>
2023-12-12 13:45:43 +0200

    Added auditing to Notes model with interceptor

--- OnlineNotes/Models/Note.cs
+++ OnlineNotes/Models/Note.cs
@@ -1,9 +1,10 @@
 ﻿using OnlineNotes.Models.Enums;
+using OnlineNotes.Models.Interfaces;
 using System.ComponentModel.DataAnnotations;
 
 namespace OnlineNotes.Models
 {
-    public class Note
+    public class Note : IAuditable
     {
         public int Id { get; set; }
         public string UserId { get; set; } = null!;
@@ -14,8 +15,11 @@ namespace OnlineNotes.Models
         public NoteStatus Status { get; set; }
         public ICollection<NoteRating> Ratings { get; } = new List<NoteRating>();
         public float? AvgRating { get; set; }
+
+        // IAuditable properties
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime CreationDate { get; set; }
+        public DateTime? ModificationDate { get; set; }
 
         public Note(string title, string contents, NoteStatus status)
         {
