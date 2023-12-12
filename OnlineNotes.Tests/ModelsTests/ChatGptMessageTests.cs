1ec3ebd9945c1c698679f5ccd73018fd7f9b9b17
vabalas <vabalas.balys@gmail.com>
2023-12-12 17:30:18 +0200

    chat gpt message test

---
+++ OnlineNotes.Tests/ModelsTests/ChatGptMessageTests.cs
@@ -0,0 +1,32 @@
+﻿using OnlineNotes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineNotes.Tests.ModelsTests
+{
+    public class ChatGptMessageTests
+    {
+        public ChatGptMessageTests() 
+        {
+
+        }
+
+        [Fact]
+        public void ChatGptMeessage_Success()
+        {
+            //Arange
+            var message = new ChatGptMessage();
+            //Act
+            message.Id = 1;
+            message.Content = "test";
+            message.IsUser = true;
+            message.Timestamp = DateTime.Now;
+            //Assert
+            Assert.NotNull(message);
+            Assert.NotNull(message.Content);
+        }
+    }
+}
