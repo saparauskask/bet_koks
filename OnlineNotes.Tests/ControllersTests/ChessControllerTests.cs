fd88087eb8c94fdbfcc1ea785378d411bbd3735a
vabalas <vabalas.balys@gmail.com>
2023-12-12 17:10:11 +0200

    some tests

--- OnlineNotes.Tests/ControllersTests/ChessControllerTests.cs
+++ OnlineNotes.Tests/ControllersTests/ChessControllerTests.cs
@@ -1,54 +1,42 @@
 ﻿using FakeItEasy;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OnlineNotes.Controllers;
 using OnlineNotes.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OnlineNotes.Tests.ControllersTests
 {
     public class ChessControllerTests
     {
         private readonly ChessController _chessController;
-        private readonly ReferencesRepository _referencesRepository;
         private readonly HttpClient _httpClient;
         public ChessControllerTests()
         {
-            //_referencesRepository = A.Fake<ReferencesRepository>();
             _httpClient = A.Fake<HttpClient>();
 
             // Configure the behavior of fakeHttpClient if needed
 
-            //_chessController = new ChessController(_fakeReferencesRepository, _fakeHttpClient);
+            _chessController = new ChessController(_httpClient);
         }
 
         [Fact]
-        public Task ChessController_Index_ReturnsSuccessAsync()
+        public async Task ChessController_Index_ReturnsSuccessAsync()
         {
-            /*
-           // Arrange
-           var expectedHtmlBoard = "<html>YourMockedHtmlBoard</html>";
+            // Arrange
+            var expectedHtmlBoard = "Mocked HTML Board";
 
-           var responseMessage = new HttpResponseMessage(HttpStatusCode.OK)
-           {
-               Content = new StringContent(expectedHtmlBoard),
-           };
+            //A.CallTo(() => _httpClient.GetAsync(A<Uri>._))
+                //.Returns(new HttpResponseMessage
+                //{
+                    //StatusCode = HttpStatusCode.OK,
+                    //Content = new StringContent(expectedHtmlBoard),
+                //});
 
-           A.CallTo(() => _fakeHttpClient.GetAsync(A<string>._, A<CancellationToken>._))
-               .Returns(Task.FromResult(responseMessage));
+            // Act
+            //var result = await _chessController.Index();
 
-           // Act
-           var result = await _chessController.Index();
-           */
-            var result = "hello";
             // Assert
-            Assert.NotNull(result);
-            return Task.CompletedTask;
+            //var viewResult = Assert.IsType<ViewResult>(result);
         }
     }
 }
